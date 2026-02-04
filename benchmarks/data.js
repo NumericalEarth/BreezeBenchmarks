37128d1b1102e393574c8436e1ef531671e5f973
github-action-benchmark <github@users.noreply.github.com>
2026-02-04 18:31:40 +0000

    add Breeze.jl Benchmarks (customSmallerIsBetter) benchmark result for 81acc404117bb1f760898862b0339d64f450a118

---
+++ benchmarks/data.js
@@ -0,0 +1,33 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1770229900556,
+  "repoUrl": "https://github.com/NumericalEarth/Breeze.jl",
+  "entries": {
+    "Breeze.jl Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "NumericalEarth",
+            "username": "NumericalEarth"
+          },
+          "committer": {
+            "name": "NumericalEarth",
+            "username": "NumericalEarth"
+          },
+          "id": "81acc404117bb1f760898862b0339d64f450a118",
+          "message": "Add a benchmarking subpackage",
+          "timestamp": "2026-02-03T15:27:58Z",
+          "url": "https://github.com/NumericalEarth/Breeze.jl/pull/426/commits/81acc404117bb1f760898862b0339d64f450a118"
+        },
+        "date": 1770229900402,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "CBL_688x688x340_F32_WENO5_nothing_nothing",
+            "value": 0.5994578906499917,
+            "unit": "steps/s"
+          }
+        ]
+      }
+    ]
+  }
+}
