ab00d59a4a203d67df0301ad6ea963ce9b65a606
github-action-benchmark <github@users.noreply.github.com>
2026-02-05 02:21:55 +0000

    add Breeze.jl Benchmarks (customBiggerIsBetter) benchmark result for 8d43b2219d5849024f2934429a46cbd8e2356802

---
+++ data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1770258115468,
+  "repoUrl": "https://github.com/NumericalEarth/Breeze.jl",
+  "entries": {
+    "Breeze.jl Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "gregory.leclaire.wagner@gmail.com",
+            "name": "Gregory L. Wagner",
+            "username": "glwagner"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "8d43b2219d5849024f2934429a46cbd8e2356802",
+          "message": "Add a benchmarking subpackage (#426)",
+          "timestamp": "2026-02-05T02:11:08Z",
+          "tree_id": "b6cd687e79d2c94ca4d71b0f1e43cf5b1550dd51",
+          "url": "https://github.com/NumericalEarth/Breeze.jl/commit/8d43b2219d5849024f2934429a46cbd8e2356802"
+        },
+        "date": 1770258115341,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "CBL_F32/WENO5/NVIDIA L4/128x128x128",
+            "value": 133764848.08011279,
+            "unit": "points/s"
+          },
+          {
+            "name": "CBL_F32/WENO5/NVIDIA L4/384x384x256",
+            "value": 117276968.35235105,
+            "unit": "points/s"
+          },
+          {
+            "name": "CBL_F32/WENO5/NVIDIA L4/768x768x256",
+            "value": 99892583.22570062,
+            "unit": "points/s"
+          }
+        ]
+      }
+    ]
+  }
+}
